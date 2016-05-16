79ae8c397cbaeb0fbf1e902087ccd75f5aa82ecb
Hayko Koryun <haykokoryun@gmail.com>
2016-05-17 03:27:53 +0400

    added intent colors

--- generator.js
+++ generator.js
@@ -26,25 +26,46 @@ var styles =
 
 var output = "";
 
-for(var skill in data)
+// process skills
+for(var skill in data.skill)
 {
   for(var i = 0; i < styles.length; ++i)
   {
     for(var j = 0; j < colors.length; ++j)
     {
       var style = {};
-      style[styles[i]] = data[skill][colors[j].color];
+      style[styles[i]] = data.skill[skill][colors[j].color];
 
       output +=
-        "." + data[skill].csstag
+        "." + data.skill[skill].csstag
         + "." + "apply-" + styles[i]
         + colors[j]["modifier"]
         + "{"
         + styles[i]
         + ":"
-        + data[skill][colors[j].color]
+        + data.skill[skill][colors[j].color]
         + "}";
     }
+  }
+}
+
+// process intents
+for(var intent in data.intent)
+{
+  for(var i = 0; i < styles.length; ++i)
+  {
+    var style = {};
+    var color = data.intent[intent];
+    style[styles[i]] = color;
+
+    output +=
+      "." + intent
+      + "." + "apply-" + styles[i]
+      + "{"
+      + styles[i]
+      + ":"
+      + color
+      + "}";
   }
 }
 
